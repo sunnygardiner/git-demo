8de118e3028d62c460441b00410e5dff9a066e5f
sunnygardiner <56595690+sunnygardiner@users.noreply.github.com>
2023-12-20 16:42:34 +0100

    added method addMethod by developer 2

--- src/main/java/com/sgtech/app/git_demo/App.java
+++ src/main/java/com/sgtech/app/git_demo/App.java
@@ -8,6 +8,9 @@ public class App
 			System.out.println("welcome");
 		}
 	}
+	public void addMethod(){
+		 System.out.println( "new method" );
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );

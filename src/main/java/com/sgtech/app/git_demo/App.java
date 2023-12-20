efe115fbc8e4cc2b0bbb20c6a85bb0040c1b6d8c
Sunny.G <sunatsun28@gmail.com>
2023-12-20 16:12:50 +0100

    added if condition to check the data

--- src/main/java/com/sgtech/app/git_demo/App.java
+++ src/main/java/com/sgtech/app/git_demo/App.java
@@ -8,6 +8,9 @@ public class App
 {
 	public void addData(String data) {
 		System.out.println("data +"+data);
+		if(data.equals("sg tech")) {
+			System.out.println("welcome");
+		}
 	}
     public static void main( String[] args )
     {
